64476e7528abb210d5d518ea3b082b3182cef1d4
carly <carly@noukod.com>
2018-09-20 15:45:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size > 2
+    last_value = array.pop
+    array.join(', ') + ", and #{last_value}"
+  elsif array.size == 2
+    "#{array.first} and #{array.last}"
+  else
+    array.join
+  end
 end
